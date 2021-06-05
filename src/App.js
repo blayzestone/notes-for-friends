e2278550d641d87715222598b0e101e6c8f8c3b1
Blayze <blayzestone@protonmail.com>
2021-06-04 21:13:54 -0700

    Setup pages and nav bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import { Link, Switch, Route } from "react-router-dom";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <header className="w-full flex justify-between items-center bg-purple-600 text-white py-3 px-6 shadow-md">
+        <Link className="text-2xl hover:opacity-50" to="/">
+          <h1>Notes for Friends</h1>
+        </Link>
+        <nav className="w-1/6 flex justify-between">
+          <Link className="hover:opacity-50" to="/login">
+            Login
+          </Link>
+          <Link className="hover:opacity-50" to="/signup">
+            Sign Up
+          </Link>
+          <Link className="hover:opacity-50" to="/settings">
+            Settings
+          </Link>
+        </nav>
+      </header>
+      <Switch>
+        <Route path="/user/:id">user</Route>
+        <Route path="/settings">Settings</Route>
+        <Route path="/login">Login</Route>
+        <Route path="/signup">Sign Up</Route>
+        <Route path="/">Home</Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
